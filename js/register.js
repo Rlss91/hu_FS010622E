775627e63b511487748e2639f7c6124ac921ac25
Rlss91 <rlss91@gmail.com>
2022-06-15 20:54:00 +0300

    register age

--- js/register.js
+++ js/register.js
@@ -11,31 +11,34 @@ function handleSubmit(ev) {
   console.log("your lastname is: " + lastname);
   console.log("your age is: " + age);
   if (area == "South") {
-    price = basePrice * maam;
+    price = basePrice;
   }
   if (area == "Center") {
-    price = basePrice * 1.5 * maam;
+    price = basePrice * 1.5;
   }
   if (area == "North") {
-    price = basePrice * 1.2 * maam;
+    price = basePrice * 1.2;
   }
   if (area == "Lowland") {
-    price = basePrice * 1.3 * maam;
+    price = basePrice * 1.3;
   }
-  //   switch (area) {
-  //     case "South":
-  //       price = basePrice * maam;
-  //       break;
-  //     case "Center":
-  //       price = basePrice * 1.5 * maam;
-  //       break;
-  //     case "North":
-  //       price = basePrice * 1.2 * maam;
-  //       break;
-  //     case "Lowland":
-  //       price = basePrice * 1.3 * maam;
-  //       break;
-  //   }
+  if (age <= 17) {
+    alert(
+      "Dear " +
+        firstname +
+        " " +
+        lastname +
+        " man you are not allowed to register!"
+    );
+    return;
+  }
+  if (age >= 18 && age <= 39) {
+    price *= 1.3; // price = price * 1.3;
+  }
+  if (age >= 40 && age <= 59) {
+    price *= 1.2; // price = price * 1.3;
+  }
+  price *= maam;
   console.log("you have to pay: " + price);
   let alertMsg = "your name is: " + firstname;
   alertMsg += "\nyour lastname is: " + lastname; // alertMsg = alertMsg + "your lastname is: " + lastname;
