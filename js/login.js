3e5c2551d5f665a090c9a41ad4102b352ea1e83a
Rlss91 <rlss91@gmail.com>
2022-06-15 20:16:18 +0300

    register many ifs

--- js/login.js
+++ js/login.js
@@ -2,14 +2,20 @@ function handleBtnClick() {
   console.log("you pressed the button");
   let emailValue = document.getElementById("emailInput").value;
   let passwordValue = document.getElementById("passwordInput").value;
-  if (emailValue == "1@2.com") {
-    console.log("your email in our system");
-    if (passwordValue == "1") {
-      console.log("your email and password are correct");
-    } else {
-      console.log("password invalid");
-    }
+  // if (emailValue == "1@2.com") {
+  //   console.log("your email in our system");
+  //   if (passwordValue == "1") {
+  //     console.log("your email and password are correct");
+  //   } else {
+  //     console.log("password invalid");
+  //   }
+  // } else {
+  //   console.log("email invalid");
+  // }
+  if (emailValue == "1@2.com" && passwordValue == "1") {
+    console.log("your email and password are correct");
+    window.location.href = "register.html";
   } else {
-    console.log("email invalid");
+    console.log("email or password invalid");
   }
 }
---
+++ js/register.js
@@ -0,0 +1,26 @@
+function handleSubmit(ev) {
+  ev.preventDefault(); //stop refresh
+  let firstname = document.getElementById("nameInput").value;
+  let lastname = document.getElementById("lastNameInput").value;
+  let age = document.getElementById("ageInput").value;
+  let area = document.getElementById("areasInput").value;
+  let price;
+  const basePrice = 1000;
+  const maam = 1.17;
+  console.log("your name is: " + firstname);
+  console.log("your lastname is: " + lastname);
+  console.log("your age is: " + age);
+  if (area == "South") {
+    price = basePrice * maam;
+  }
+  if (area == "Center") {
+    price = basePrice * 1.5 * maam;
+  }
+  if (area == "North") {
+    price = basePrice * 1.2 * maam;
+  }
+  if (area == "Lowland") {
+    price = basePrice * 1.3 * maam;
+  }
+  console.log("you have to pay: " + price);
+}
