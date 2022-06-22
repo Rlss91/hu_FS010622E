f616e3a6d7db6ec1a8573d07db8708dbc5a0b03d
Rlss91 <rlss91@gmail.com>
2022-06-22 21:07:17 +0300

    targil

--- js/targil.js
+++ js/targil.js
@@ -2,8 +2,11 @@ function handleCalc() {
   let n1 = document.getElementById("number1").value;
   let n2 = document.getElementById("number2").value;
   let sum = parseFloat(n1) + parseFloat(n2);
+  let sum2 = parseFloat(n1) - parseFloat(n2);
+  let sum3 = parseFloat(n1) * parseFloat(n2);
+  let sum4 = parseFloat(n1) / parseFloat(n2);
   console.log(n1 + " + " + n2 + " = " + sum);
+  console.log(n1 + " - " + n2 + " = " + sum2);
+  console.log(n1 + " * " + n2 + " = " + sum3);
+  console.log(n1 + " / " + n2 + " = " + sum4);
 }
-
-console.log("abc" + "def");
-//abcdef
